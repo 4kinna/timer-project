fff16ccedbc3d966ccb757d915ab120af3674ae4
Annika <a.staalhandske@gmail.com>
2022-10-17 11:29:49 +0200

    new time component

---
+++ client/src/component/Time.js
@@ -0,0 +1,15 @@
+export const Time = (props) => {
+  return (
+    <h1>
+      <span className="hour">
+        {("0" + Math.floor((props.time / 60000) % 60)).slice(-2)}:
+      </span>
+      <span className="min">
+        {("0" + Math.floor((props.time / 1000) % 60)).slice(-2)}:
+      </span>
+      <span className="sec">{("0" + ((props.time / 10) % 100)).slice(-2)}</span>
+    </h1>
+  );
+};
+
+export default Time;
--- client/src/component/Timer.js
+++ client/src/component/Timer.js
@@ -1,66 +1,54 @@
 import { useState, useRef } from "react";
-
+import Time from "./Time.js";
 
 const Timer = () => {
   const [time, setTime] = useState(0);
-  const [isActive, setIsActive] = useState(false)
-  const [isPaused, setIsPaused] = useState(false)
-  const countRef = useRef(null)
+  const [isActive, setIsActive] = useState(false);
+  const [isPaused, setIsPaused] = useState(false);
+  const countRef = useRef(null);
 
   const handleStart = () => {
-    setIsActive(true)
-    setIsPaused(true)
+    setIsActive(true);
+    setIsPaused(true);
     countRef.current = setInterval(() => {
-      setTime((time) => time + 10)
-    }, 1000)
-  }
+      setTime((time) => time + 10);
+    }, 1000);
+  };
 
   const handlePause = () => {
-    clearInterval(countRef.current)
-    setIsPaused(false)
-  }
+    clearInterval(countRef.current);
+    setIsPaused(false);
+  };
 
   const handleReset = () => {
-    clearInterval(countRef.current)
-  setIsActive(false)
-  setIsPaused(false)
-  setTime(0)
-  }
+    clearInterval(countRef.current);
+    setIsActive(false);
+    setIsPaused(false);
+    setTime(0);
+  };
 
   const handleResume = () => {
-    setIsPaused(true)
+    setIsPaused(true);
     countRef.current = setInterval(() => {
-      setTime((time) => time + 10)
-    }, 1000)
-  }
+      setTime((time) => time + 10);
+    }, 1000);
+  };
 
   return (
     <div>
       <label>TIMER</label>
-      <h1>
-        <span className="hour">
-          {("0" + Math.floor((time / 60000) % 60)).slice(-2)}:
-        </span>
-        <span className="min">
-          {("0" + Math.floor((time / 1000) % 60)).slice(-2)}:
-        </span>
-        <span className="sec">{("0" + ((time / 10) % 100)).slice(-2)}</span>
-      </h1>
-
+      <Time time={time} />
       <div className="buttons">
-      {
-            !isActive && !isPaused ?
-              <button onClick={handleStart}>Start</button>
-              : (
-                isPaused ? <button onClick={handlePause}>Pause</button> :
-                  <button onClick={handleResume}>Resume</button>
-              )
-          }
+        {!isActive && !isPaused ? (
+          <button onClick={handleStart}>Start</button>
+        ) : isPaused ? (
+          <button onClick={handlePause}>Pause</button>
+        ) : (
+          <button onClick={handleResume}>Resume</button>
+        )}
         <button>Stop ?? </button>
-        <button onClick={handleReset} >Reset</button>
-        </div>
-
-
+        <button onClick={handleReset}>Reset</button>
+      </div>
     </div>
   );
 };
