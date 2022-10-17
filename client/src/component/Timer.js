aa7381170b9f90e2870ce8c0135821f5a2179dd1
martinacurl <martinacurl@hotmail.com>
2022-10-17 09:02:30 +0200

    Buttons

--- client/src/component/Timer.js
+++ client/src/component/Timer.js
@@ -1,7 +1,38 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
+
 
 const Timer = () => {
   const [time, setTime] = useState(0);
+  const [isActive, setIsActive] = useState(false)
+  const [isPaused, setIsPaused] = useState(false)
+  const countRef = useRef(null)
+
+  const handleStart = () => {
+    setIsActive(true)
+    setIsPaused(true)
+    countRef.current = setInterval(() => {
+      setTime((time) => time + 10)
+    }, 1000)
+  }
+
+  const handlePause = () => {
+    clearInterval(countRef.current)
+    setIsPaused(false)
+  }
+
+  const handleReset = () => {
+    clearInterval(countRef.current)
+  setIsActive(false)
+  setIsPaused(false)
+  setTime(0)
+  }
+
+  const handleResume = () => {
+    setIsPaused(true)
+    countRef.current = setInterval(() => {
+      setTime((time) => time + 10)
+    }, 1000)
+  }
 
   return (
     <div>
@@ -15,6 +46,21 @@ const Timer = () => {
         </span>
         <span className="sec">{("0" + ((time / 10) % 100)).slice(-2)}</span>
       </h1>
+
+      <div className="buttons">
+      {
+            !isActive && !isPaused ?
+              <button onClick={handleStart}>Start</button>
+              : (
+                isPaused ? <button onClick={handlePause}>Pause</button> :
+                  <button onClick={handleResume}>Resume</button>
+              )
+          }
+        <button>Stop ?? </button>
+        <button onClick={handleReset} >Reset</button>
+        </div>
+
+
     </div>
   );
 };
