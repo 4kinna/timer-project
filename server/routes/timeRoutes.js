285023dc9dc8445c88d4aa46b1e98986f3f42d99
Annika <a.staalhandske@gmail.com>
2022-10-21 13:29:36 +0200

    created fetch functions to frontend, fetch list of posts and fetch post and add to list of post

--- server/routes/timeRoutes.js
+++ server/routes/timeRoutes.js
@@ -1,36 +1,39 @@
-import express from 'express';
-import { PrismaClient } from '@prisma/client'        
-const prisma = new PrismaClient()
-
+import express from "express";
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 
 const route = express.Router();
 
 route.get("/", async (req, res) => {
+  const savedtimes = await prisma.posts.findMany({
+    select: {
+      id: true,
+      note: true,
+      time: true,
+      savedAt: true,
+    },
+  });
 
-    const savedtimes = await prisma.posts.findMany({
-        select: {
-            id: true,
-            note:true,
-            time: true,
-            savedAt: true
-        }
-    })
-
-    res.json(savedtimes)
-
-})
+  res.json(savedtimes);
+});
 
 route.post("/", async (req, res) => {
-    const savedtime = await prisma.posts.create({
-        data: {
-            id: req.body.id,
-            note:req.body.note,
-            time: req.body.time,
+  const savedtime = await prisma.posts.create({
+    data: {
+      note: req.body.note,
+      time: req.body.time,
+    },
+  });
+  res.json(savedtime);
+});
 
-        }
-    })
-    res.json(savedtime)
-
-})
+route.delete("/:id", async (req, res) => {
+  await prisma.posts.delete({
+    where: {
+      id: parseInt(req.params.id),
+    },
+  });
+  res.sendStatus(204);
+});
 
 export default route;
